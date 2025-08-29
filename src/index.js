b73cb029070531b440b078b1c2a5333069137097
lal28 <lucasprolopes@gmail.com>
2025-08-28 21:04:57 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+import usersRouter from "./routes/users.js";
+
+const app = express();
+app.use(bodyParser.json());
+
+// Todas as rotas de usuários ficam em /api/v1/users
+app.use("/api/v1/users", usersRouter);
+
+app.listen(3000, () => {
+  console.log("API rodando em http://localhost:3000 🚀");
+});
