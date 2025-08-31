3bacd4d67c6c1cccbf5b98676054b948c90f874b
Pedro9185 <pedrocatarina.ip@gmail.com>
2025-08-31 09:54:33 -0300

    feat: adicionado swagger-api-documentation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import usersRouter from "./routes/users.js";
+import swaggerRoute from "./routes/swagger.route.js";
 
 const app = express();
 app.use(bodyParser.json());
 
 // Todas as rotas de usuários ficam em /api/v1/users
 app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/docs", swaggerRoute);
 
 app.listen(3000, () => {
   console.log("API rodando em http://localhost:3000 🚀");
